43261a37fe98756008114423ed73c13bbe0beb46
Federico Cabaña <federicogabriel12@hotmail.com>
2024-09-30 19:26:30 -0300

    Eliminacion de conflictos de clase obsoleta producto

--- backend/src/services.py
+++ backend/src/services.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 #from sqlalchemy.future import select
 from src import schemas, models
-from src.models import Producto
 from fastapi import HTTPException
 from database import SessionLocal
 
