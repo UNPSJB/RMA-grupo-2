696bcd11e6dbf4dc92ff423bd72ce566157df544
Zappe02 <patriciozappellini02@gmail.com>
2024-10-16 11:03:30 -0300

    lo mismo que el anterior xd

--- frontend/src/components/Tables/TableOne.tsx
+++ frontend/src/components/Tables/TableOne.tsx
@@ -42,7 +42,7 @@ const TableOne: React.FC = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Temperaturas por Nodos
+        Ultimas 5 Temperatura Recibidas
       </h4>
 
       <div className="flex flex-col">
