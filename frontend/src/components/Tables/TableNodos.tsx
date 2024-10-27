b74208beb8ad400e40397056c5a32dc2d05c6395
Federico Cabaña <federicogabriel12@hotmail.com>
2024-10-27 14:06:16 -0300

    Resolucion de Modificar del lado del frontend

---
+++ frontend/src/components/Tables/TableNodos.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+interface Nodo {
+  id: number;
+  posicionx: number;
+  posiciony: number;
+}
+
+interface onEditUptMode {
+  onEditUptMode: (nodo: Nodo) => void;
+}
+
+const TableNodos: React.FC <{ onEditUptMode: (nodo: Nodo) => void }> = ({ onEditUptMode })=> {
+  const [nodosData, setNodosData] = useState<Nodo[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const eliminarNodo = async (id: number): Promise<void> => {
+    try {
+      const response = await fetch(`http://localhost:8000/nodo/${id}`, {
+          method: "DELETE", 
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id }), 
+      });
+      alert("Nodo eliminado con exito.");
+      if (response.ok) {
+          const responseData = await response.json(); 
+          console.log("Nodo eliminado:", responseData);
+      } else {
+          const errorData = await response.json(); 
+          console.error("Error del servidor:", errorData);
+      }
+  } catch (error) {
+      console.error("Error:", error); 
+  }}
+
+  
+  const modificarNodo = async (nodo: Nodo): Promise<void> => {
+    try {      
+      const response = await fetch(`http://localhost:8000/nodo/${nodo.id}`, {
+          method: "PUT", 
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ nodo }), 
+      });
+      if (response.ok) {
+          alert("Nodo modificado con exito.");
+          const responseData = await response.json(); 
+          console.log("Nodo modificado:", responseData);
+      } else {
+          const errorData = await response.json(); 
+          console.error("Error del servidor:", errorData);
+      }
+  } catch (error) {
+      console.error("Error:", error); 
+  }}
+
+
+  useEffect(() => {
+    const obtenerNodos = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/nodos/');
+        const nodos = response.data;
+        setNodosData(nodos);
+      } catch (error) {
+        console.error('Error al obtener los nodos:', error);
+        setError('Error al cargar los datos.');
+      }
+    };
+
+    obtenerNodos();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (nodosData.length === 0) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+        Nodos de la red
+      </h4>
+
+      <div className="flex flex-col">
+        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+          <div className="p-2.5 xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">Id</h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">X</h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">Y</h5>
+          </div>
+        </div>
+
+        {nodosData.map((nodo) => (
+          <div
+            className={`grid grid-cols-3 sm:grid-cols-5 border-b border-stroke dark:border-strokedark`}
+            key={nodo.id}
+          >
+          <div className="flex items-center gap-3 p-2.5 xl:p-5">
+              <p className="text-black dark:text-white">{nodo.id}</p>
+          </div>
+          <div className="flex items-center gap-3 p-2.5 xl:p-5">
+            <p className="text-black dark:text-white">{nodo.posicionx}</p>
+          </div>
+          <div className="flex items-center justify-center p-2.5 xl:p-5">
+            <p className="text-black dark:text-white">{ nodo.posiciony}</p>
+          </div>
+          <div className="flex items-center justify-center p-2.5 xl:p-5 space-x-17">
+          <button
+            className="bg-yellow-500 text-white px-4 py-2 rounded"
+            onClick={() => onEditUptMode(nodo)}
+          >
+            Editar
+          </button>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded"
+            onClick={() => eliminarNodo(nodo.id)} 
+          >
+            Eliminar
+          </button>
+        </div>
+        </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TableNodos;
