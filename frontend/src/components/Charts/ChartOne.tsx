bc989c86b88750560b2279dbb68ac12cb9f97ad0
R4WR-XD <ashpanda64@gmail.com>
2024-11-08 18:05:10 -0300

    Chart one filtra bien pero no carga bien los datos al grafico

--- frontend/src/components/Charts/ChartOne.tsx
+++ frontend/src/components/Charts/ChartOne.tsx
@@ -6,19 +6,28 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
 
-const allProducts = [
+interface Medicion {
+  id: number;
+  nodo: number;
+  tipo: number; 
+  dato: number;
+  tiempo: string;
+  error: boolean;
+}
+
+/*const allProducts = [
   { name: 'Nodo 1', data: [] }, // Comenzamos con un array vacío
   { name: 'Nodo 2', data: [34, 22, 41, 27, 50, 19, 64, 53, 31, 48, 22, 40, 58, 33, 25, 46, 69, 37, 55, 44, 26, 60, 43, 28, 52] },
   { name: 'Nodo 3', data: [20, 15, 30, 22, 40, 25, 55, 50, 20, 30, 50, 40, 60, 25, 45, 35, 55, 65, 25, 35, 45, 55, 30, 60, 40] },
   { name: 'Nodo 4', data: [25, 20, 40, 18, 60, 22, 53, 47, 35, 25, 44, 55, 62, 20, 44, 38, 50, 64, 37, 50, 48, 64, 34, 56, 42] },
   { name: 'Nodo 5', data: [15, 25, 35, 45, 55, 20, 40, 60, 30, 50, 35, 45, 55, 60, 70, 20, 45, 55, 60, 65, 70, 25, 35, 45, 50] },
-];
+];*/
 
 
 
 const ChartOne: React.FC = () => {
-  const [nodo1Data, setNodo1Data] = useState<number[]>([]);
-  const [selectedProducts, setSelectedProducts] = useState<number[]>([0, 1]);
+  const [filteredData, setFilteredData] = useState<Medicion[]>([]);
+  const [medicionData, setMedicionData] = useState<Medicion[]>([]);
   const [fechas, setFechas] = useState<string[]>([]);
   const [valores, setValores] = useState<number[]>([]);
 
@@ -26,7 +35,7 @@ const ChartOne: React.FC = () => {
     legend: { show: true },
     chart: { type: 'area', height: 350 },
     xaxis: {
-      categories: Array.from({ length: fechas.length }, (_, i) => i.toString()), // Mantener las categorías fijas
+      categories: Array.from({ length: filteredData.length }, (_, i) => i.toString()), // Mantener las categorías fijas
       labels: { show: true },
       title: { text: 'Hora' },
     },
@@ -86,8 +95,7 @@ const ChartOne: React.FC = () => {
   ];
   const selectDataTypeOptions = [
     { value: 1, label: "Temperatura" },
-    { value: 2, label: "Humedad" },
-    { value: 3, label: "Presión" },
+    { value: 25, label: "Altura" },
   ];
 
   const [selectedNode, setSelectedNode] = useState(selectNodeOptions[0].value);
@@ -109,22 +117,36 @@ const ChartOne: React.FC = () => {
 
   const handleSearch = async () => {
 
-    // Llamada a la función que obtiene los datos con los filtros
-    const filtro = {
-      nodo: selectedNode,
-      tipo: selectedDataType,
-      fechaDesde: startDate,
-      fechaHasta: endDate
-    }
-    const response = await fetch('http://localhost:8000/medicion/filtrar', {
-      method: "POST",
-      headers: {"Content-Type": "application/json",},
-      body: JSON.stringify(filtro),
-    });
-    if (response.ok){
-      const data = await response.json();
-      setFechas(data.fechas);
-      setValores(data.valores);
+    try {
+      const response = await axios.get('http://localhost:8000/medicion/');
+      let data = response.data;
+      console.log({data});
+      setMedicionData(data)
+      if (selectedNode !== null) {
+        data = data.filter((item: { nodo: number; }) => item.nodo === Number(selectedNode));
+      }
+  
+      if (selectedDataType === 1) {
+        data = data.filter((item: { tipo: number; }) => item.tipo === 1 || item.tipo === 2);
+      } else if (selectedDataType === 25) {
+        data = data.filter((item: { tipo: number; }) => item.tipo === 25);
+      }
+  
+      if (startDate) {
+        data = data.filter((item: { tiempo: string | number | Date; }) => new Date(item.tiempo) >= new Date(startDate));
+      }
+  
+      if (endDate) {
+        data = data.filter((item: { tiempo: string | number | Date; }) => new Date(item.tiempo) <= new Date(endDate));
+      }
+      setFilteredData(data);
+      setFechas(filteredData.map((d: Medicion) => d.tiempo));
+      setValores(filteredData.map((d: Medicion) => d.dato));
+      console.log({filteredData});
+      console.log({fechas});
+      console.log({valores});
+    } catch (error) {
+      console.error('Error al obtener las mediciones:', error);
     }
     
 
@@ -186,7 +208,7 @@ const ChartOne: React.FC = () => {
         }}
         series={[{
           name: selectDataTypeOptions[selectedDataType].label,
-          data: valores  // Ajusta con los valores de las mediciones filtradas
+          data: valores // Ajusta con los valores de las mediciones filtradas
         }]}
         type="area"
         height={350}
