3bd1e6a9fed577434bb15ad3e922e272200edf0a
zappe02 <patriciozappellini02@gmail.com>
2024-11-11 16:56:05 -0300

    cambios login y boton para exportar a pdf

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -11,7 +11,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #para encriptar pass
+pwd_context = CryptContext(schemes=["bcrypt"]) #, deprecated="auto") #para encriptar pass
 
 
 load_dotenv()#.env
