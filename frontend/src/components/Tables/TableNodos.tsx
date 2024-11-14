5aba129a9e789547570abf21d84933489e5bbfb4
Shojan99 <mirojoan99@gmail.com>
2024-11-14 14:27:13 -0300

    msj completos

--- frontend/src/components/Tables/TableNodos.tsx
+++ frontend/src/components/Tables/TableNodos.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AlertPopup from '../../components/popUp';
+import AlertPopup from '../popUp';
 
 interface Nodo {
   id: number;
@@ -58,7 +58,7 @@ const TableNodos: React.FC<TableNodosProps> = ({ nodos, setNodos, onEditUptMode 
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Nodos totales
+        Lista de nodos
       </h4>
       
       {}
@@ -91,7 +91,7 @@ const TableNodos: React.FC<TableNodosProps> = ({ nodos, setNodos, onEditUptMode 
 
         {nodos.map((nodo) => (
           <div className="grid grid-cols-3 sm:grid-cols-5 border-b border-stroke dark:border-strokedark" key={nodo.id}>
-            <div className="flex items-center gap-3 p-2.5 xl:p-5">
+            <div className="flex items-center  gap-3 p-2.5 xl:p-5">
               <p className="text-black dark:text-white">{nodo.nombre}</p>
             </div>
             <div className="flex items-center gap-3 p-2.5 xl:p-5">
@@ -100,7 +100,7 @@ const TableNodos: React.FC<TableNodosProps> = ({ nodos, setNodos, onEditUptMode 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
               <p className="text-black dark:text-white">{nodo.posiciony}</p>
             </div>
-            <div className="flex items-center gap-3 p-2.5 xl:p-5">
+            <div className="flex items-center justify-center gap-3 p-2.5 xl:p-5">
               <p className="text-black dark:text-white">{nodo.descripcion}</p>
             </div>
             <div className="flex items-center justify-center p-2.5 xl:p-5 space-x-5">
